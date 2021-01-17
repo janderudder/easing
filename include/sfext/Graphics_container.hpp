672ded83f8d013f5386ede47d46ef0f66a918406
janderudder <39810873+janderudder@users.noreply.github.com>
2021-01-18 00:46:01 +0100

    feat - update lib code (sfext/Graphics_container)

--- include/sfext/Graphics_container.hpp
+++ include/sfext/Graphics_container.hpp
@@ -19,14 +19,14 @@ public:
         sf::Transform,
         std::reference_wrapper<sf::Transform>,
         std::reference_wrapper<sf::Transform const>,
-        std::function<sf::Transform()>
+        sf::Transform(*)()
     >;
 
 private:
     struct value_type
     {
-        std::reference_wrapper<sf::Drawable const>  entity;
-        transform_sumtype                           transform_variant;
+        std::reference_wrapper<sf::Drawable const>  nm_drawable_ref;
+        transform_sumtype                           nm_transform_variant;
 
         value_type(sf::Drawable const&, transform_sumtype ={}) noexcept;
     };
@@ -37,14 +37,42 @@ public:
     Graphics_container() = default;
     Graphics_container(std::initializer_list<value_type>) noexcept;
 
+     template <template <class, class...> class Container, class Drw, class...O>
+    Graphics_container(Container<Drw, O...> const& container) noexcept;
+
     void add(sf::Drawable const&, transform_sumtype = {});
     void add(std::initializer_list<value_type>);
+
+     template <template <class, class...> class Container, class Drw, class...O>
+    void add(Container<Drw, O...> const& container);
 
     auto empty() const -> bool;
     auto size() const -> size_t;
 
+
 private:
-    void draw(sf::RenderTarget&, sf::RenderStates= sf::RenderStates::Default)
+    void draw(sf::RenderTarget&, sf::RenderStates = sf::RenderStates::Default)
         const override;
 
 };
+
+
+
+// inline impl.
+////////////////////////////////////////////////////////////////////////////////
+    template <template <class, class...> class Container, class Drw, class...O>
+Graphics_container::Graphics_container(Container<Drw, O...> const& container)
+ noexcept
+    : m_values {container.begin(), container.end()}
+{
+}
+
+
+
+    template <template <class, class...> class Container, class Drw, class...O>
+void Graphics_container::add(Container<Drw, O...> const& container)
+{
+    for (auto const& drawable : container) {
+        add(drawable);
+    }
+}
--- src/sfext/Graphics_container.cpp
+++ src/sfext/Graphics_container.cpp
@@ -6,77 +6,68 @@
 ////////////////////////////////////////////////////////////////////////////////
 namespace
 {
-    constexpr auto transform_sumtype_visitor = [](auto&& one_of_sumtype)
+constexpr auto transform_sumtype_visitor = [](auto&& one_of_sumtype) noexcept
+{
+    using ref_t = std::reference_wrapper<sf::Transform>;
+    using cref_t = std::reference_wrapper<sf::Transform const>;
+
+    using T = std::decay_t<decltype(one_of_sumtype)>;
+
+    if constexpr (std::is_invocable_v<T>)
     {
-using T = std::decay_t<decltype(one_of_sumtype)>;
-
-if constexpr (std::is_same_v<T, sf::Transform>)
-{
-    return one_of_sumtype;
-}
-else if constexpr (std::is_same_v<T, std::reference_wrapper<sf::Transform>>)
-{
-    return one_of_sumtype.get();
-}
-else if constexpr (
-    std::is_same_v<T, std::reference_wrapper<sf::Transform const>>)
-{
-    return one_of_sumtype.get();
-}
-else
-{
-    return std::invoke(one_of_sumtype);
-}
-
-    };
+        return std::invoke(one_of_sumtype);
+    }
+    else if constexpr (std::is_same_v<T, sf::Transform>)
+    {
+        return one_of_sumtype;
+    }
+    else
+    {
+        return one_of_sumtype.get();
+    }
+};
 } // ::
-
 
 
 
 // nested struct: value_type
 ////////////////////////////////////////////////////////////////////////////////
 Graphics_container::value_type::value_type(
-    sf::Drawable const& _entity,
-    transform_sumtype   _transform_variant
+    sf::Drawable const& drawable,
+    transform_sumtype   transform_variant
 ) noexcept
-    : entity            {std::cref(_entity)}
-    , transform_variant {std::move(_transform_variant)}
+    : nm_drawable_ref       {std::cref(drawable)}
+    , nm_transform_variant  {std::move(transform_variant)}
 {
 }
-
 
 
 
 // ctor
 ////////////////////////////////////////////////////////////////////////////////
-Graphics_container::Graphics_container(std::initializer_list<value_type>entries)
+Graphics_container::Graphics_container(std::initializer_list<value_type> values)
  noexcept
-    : m_values {entries}
+    : m_values {values}
 {
 }
-
 
 
 
 // mutate
 ////////////////////////////////////////////////////////////////////////////////
 void Graphics_container::add(
-    sf::Drawable const& entity,
+    sf::Drawable const& drawable,
     transform_sumtype   transform_variant
-)
-{
-    m_values.push_back({entity, std::move(transform_variant)});
+){
+    m_values.push_back({drawable, std::move(transform_variant)});
 }
 
 
 
-
-void Graphics_container::add(std::initializer_list<value_type> entries)
-{
-    m_values.insert(m_values.end(), entries);
-}
-
+// void Graphics_container::add(std::initializer_list<value_type> values)
+// {
+//     m_values.insert(m_values.end(), values);
+// }
 
 
 
@@ -89,12 +80,10 @@ auto Graphics_container::empty() const -> bool
 
 
 
-
 auto Graphics_container::size() const -> size_t
 {
     return m_values.size();
 }
-
 
 
 
@@ -103,16 +92,19 @@ auto Graphics_container::size() const -> size_t
 void Graphics_container::draw(sf::RenderTarget& target, sf::RenderStates states)
  const
  {
-    auto const& container_trans = this->getTransform();
+    states.transform *= getTransform();
 
-    for (auto const& [entity, transform_variant] : std::as_const(m_values))
+    for (auto const& [drawable_ref, transform_variant]: std::as_const(m_values))
     {
-        auto entity_states = states;
-        entity_states.transform *= container_trans;
-
-        entity_states.transform
-            *= std::visit(transform_sumtype_visitor, transform_variant);
-
-        target.draw(entity.get(), entity_states);
+        target.draw(
+            drawable_ref.get(),
+            sf::RenderStates{
+                states.blendMode,
+                states.transform
+                    * std::visit(transform_sumtype_visitor, transform_variant),
+                states.texture,
+                states.shader
+            }
+        );
     }
 }
